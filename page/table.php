56575033f1e30e9c5f5c62ca7273d530dc92ad8a
Kozim <kozim_7484443@mail.ru>
2023-10-17 10:36:27 +0500

    Таблица настроена

--- page/table.php
+++ page/table.php
@@ -3,7 +3,7 @@
         <h2 class="head__title"><?= $title ?></h2>
         <p class="head__date"><?= $date ?></p>
     </section>
-    <form action="" class="form" method="get">
+    <form action="" class="form" method="post">
         <label class="form__label">
             <span class="form__text">Количество колонок</span>
             <input type="text" class="form__input" name="col">
@@ -16,66 +16,23 @@
     </form>
 
     <div class="table">
-        <div class="table__row">
-            <div class="table__col">1</div>
-            <div class="table__col">2</div>
-            <div class="table__col">3</div>
-            <div class="table__col">4</div>
-            <div class="table__col">5</div>
-            <div class="table__col">6</div>
-            <div class="table__col">7</div>
-            <div class="table__col">8</div>
-            <div class="table__col">9</div>
-            <div class="table__col">10</div>
-        </div>
-        <div class="table__row">
-            <div class="table__col">2</div>
-            <div class="table__col">4</div>
-            <div class="table__col">6</div>
-            <div class="table__col">8</div>
-            <div class="table__col">10</div>
-            <div class="table__col">12</div>
-            <div class="table__col">14</div>
-            <div class="table__col">16</div>
-            <div class="table__col">18</div>
-            <div class="table__col">20</div>
-        </div>
-        <div class="table__row">
-            <div class="table__col">3</div>
-            <div class="table__col">6</div>
-            <div class="table__col">9</div>
-            <div class="table__col">12</div>
-            <div class="table__col">15</div>
-            <div class="table__col">18</div>
-            <div class="table__col">21</div>
-            <div class="table__col">24</div>
-            <div class="table__col">27</div>
-            <div class="table__col">30</div>
-        </div>
-        <div class="table__row">
-            <div class="table__col">4</div>
-            <div class="table__col">8</div>
-            <div class="table__col">12</div>
-            <div class="table__col">16</div>
-            <div class="table__col">20</div>
-            <div class="table__col">24</div>
-            <div class="table__col">28</div>
-            <div class="table__col">32</div>
-            <div class="table__col">36</div>
-            <div class="table__col">40</div>
-        </div>
-        <div class="table__row">
-            <div class="table__col">5</div>
-            <div class="table__col">10</div>
-            <div class="table__col">15</div>
-            <div class="table__col">20</div>
-            <div class="table__col">25</div>
-            <div class="table__col">30</div>
-            <div class="table__col">35</div>
-            <div class="table__col">40</div>
-            <div class="table__col">45</div>
-            <div class="table__col">50</div>
-        </div>
+
+        <?
+        $rows = $_POST['row'];
+        $cols = $_POST['col'];
+        ?>
+
+        <? for ($i = 1; $i <= $rows; $i++) : ?>
+
+            <div class="table__row">
+                <? for ($x = 1; $x <= $cols; $x++) : ?>
+                    <div class="table__col">
+                        <?= $i * $x ?>
+                    </div>
+                <? endfor; ?>
+            </div>
+        <? endfor; ?>
+
     </div>
 
 
